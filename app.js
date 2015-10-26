f382e64ac983fe58c225b24064adc00e9cc44cf9
fq Chen <991glasses@fqmatoMacBook-Pro.local>
2015-10-26 18:59:51 +0800

    test

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
+
 var express = require('express');
 var app = express();
+
+var http  = require('http').Server(app);
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function(req, res) {
   res.send('hello world');
+});
+
+http.listen(80, function(){
+
 });
